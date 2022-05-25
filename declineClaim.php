e567c101c81cbd458d0a0a0a67ea1710f831f301
ZYXAdmin <ZYXAdmin@ZYCLYX.local>
2022-05-25 10:04:28 +0530

    Initial Commit - Nikhil

---
+++ declineClaim.php
@@ -0,0 +1,11 @@
+<?php
+include'connection.php';
+$client_id = $_GET["client_id"];
+ 
+$sql = "UPDATE `services` SET  `status`='Declined' WHERE `policy_number` = '$client_id'"; 
+if ($conn->query($sql) === true) {
+     header("Location: claimClient.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
